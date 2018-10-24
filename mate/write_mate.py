2c653e00fb21f5b68367437f3da52ac02a77d74f
Kevin Jensen <kevin@turtlerockweb.com>
2018-10-24 16:56:15 -0600

    updated tests

--- mate/write_mate.py
+++ mate/write_mate.py
@@ -1,7 +1,7 @@
 import struct
 
 
-def write_mate(data: list, outfile_name: str) -> str:
+def write_mate(data: list, outfile_name: str) -> None:
     if len(data) != 65536:
         if len(data) < 65536:
             print("Error 2001: Data list is not long enough. Expected 65536 but saw {}".format(len(data)))
@@ -13,4 +13,23 @@ def write_mate(data: list, outfile_name: str) -> str:
     with open(outfile_name, 'wb+') as outfile:
         for index in range(65536):
             blend_weight = int((data[index].blend_weight * 255)) & 255
-            outfile.write(struct.pack('>4B', data[index].material_0_index, data[index].material_1_index, blend_weight, data[index].unknown))
+            outfile.write(struct.pack('>4B', data[index].material_0_index, data[index].material_1_index, blend_weight,
+                                      data[index].unknown))
+
+
+def compile_mate(data: list) -> str:
+    if len(data) != 65536:
+        if len(data) < 65536:
+            print("Error 2001: Data list is not long enough. Expected 65536 but saw {}".format(len(data)))
+            exit(2001)
+
+        print("Error 2002: Data list is too long. Expected 65536 but saw {}".format(len(data)))
+        exit(2002)
+
+    mate_binary = b''
+    for index in range(65536):
+        blend_weight = int((data[index].blend_weight * 255)) & 255
+        mate_binary += struct.pack('>4B', data[index].material_0_index, data[index].material_1_index, blend_weight,
+                                   data[index].unknown)
+
+    return mate_binary
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,5 @@
+import filecmp
+import hashlib
 import json
 
 import mate
@@ -7,9 +9,10 @@ def mate_to_json():
     """
     Tests reading of mate file and exports data as a json file
     """
-    data = mate.read_mate(r"assets/5000000000.mate")
+    data = mate.read_mate("assets/5000000000.mate")
 
-    with open('output/5000000000.mate.json', 'w+') as outfile:
+    print("Saving file output/5000000000.mate.json...")
+    with open("output/5000000000.mate.json", "w+") as outfile:
         out_obj = []
         for entry in data:
             out_obj.append(entry.__dict__)
@@ -17,14 +20,44 @@ def mate_to_json():
         outfile.write(json.dumps(out_obj, indent=4, separators=(',', ': ')))
 
 
-def mate_to_binary():
-    data = mate.read_mate(r"assets/5000000000.mate")
-    mate.write_mate(data, 'output/5000000000.mate')
+def mate_to_binary_string():
+    """
+    Tests that data is recompiled correctly and matches the original file
+    """
+    data = mate.read_mate("assets/5000000000.mate")
+    binary_data = mate.compile_mate(data)
+
+    hash_md5 = hashlib.md5()
+    with open("assets/5000000000.mate", "rb") as infile:
+        for chunk in iter(lambda: infile.read(4096), b""):
+            hash_md5.update(chunk)
+
+    file_hash = hash_md5.hexdigest()
+
+    hash_md5 = hashlib.md5()
+    pos = 0
+    for chunk in iter(lambda: binary_data[pos:pos + 4096], b""):
+        pos += 4096
+        hash_md5.update(chunk)
+
+    string_hash = hash_md5.hexdigest()
+
+    print("The file and binary string are the same: {0}".format(file_hash == string_hash))
+
+
+def mate_to_binary_file():
+    """
+    Tests reading data from mate file then writes the same data back as a binary
+    """
+    data = mate.read_mate("assets/5000000000.mate")
+    mate.write_mate(data, "output/5000000000.mate")
+    print("The files are the same: {0}".format(filecmp.cmp("assets/5000000000.mate", "output/5000000000.mate")))
 
 
 def main():
-    # mate_to_json()
-    mate_to_binary()
+    mate_to_json()
+    mate_to_binary_string()
+    mate_to_binary_file()
 
 
 if __name__ == "__main__":
